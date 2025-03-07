2914d2fe5ec6f91a45522be0d991c71b53a9084f
Mercy <maritimmercy8@gmail.com>
2025-03-07 11:57:38 +0300

    defined function

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+const employee = {}
+
+function updateEmployeeWithKeyAndValue(employee,key,value) {
+    return {...employee, [key]: value}
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value) {
+    employee[key] = value
+    return employee
+
+}
+function deleteFromEmployeeByKey(employee, key) {
+    return {...employee,[key]:undefined}
+
+}
+function  destructivelyDeleteFromEmployeeByKey(employee, key) {
+    employee[key] = undefined
+    return employee
+}
